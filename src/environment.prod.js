891b673f8794bc9277efc29a9c05ef4eeabe7c8e
David Poetzsch-Heffter <davidpoetzsch@web.de>
2020-05-15 17:02:05 +0200

    changes to make firefox plugin production ready

---
+++ src/environment.prod.js
@@ -0,0 +1,20 @@
+const environmentProd = {
+    production: true,
+    endpoint: 'https://api.clockify.me/api',
+    home: 'https://clockify.me',
+    signUp: 'https://clockify.me/signup',
+    resetPassword: 'https://clockify.me/reset-password',
+    startButton:'START',
+    stopButton:'STOP',
+    webSocket: {
+        endpoint: 'wss://stomp.clockify.me',
+        clientId: 'clockify'
+    },
+    redirectUriOauthChromeExtension: "login/extension/chrome/oauth2",
+    redirectUriOauthFirefoxExtension: "login/extension/firefox/oauth2",
+    webClientId: 'your-clientId-from-google-api-console'
+};
+
+export function getEnv() {
+    return environmentProd;
+}
